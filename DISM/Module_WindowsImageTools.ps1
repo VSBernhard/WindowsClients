67082fba8f0c971f294214b6c9cd8f4281c3711f
VSBernhard <125886388+VSBernhard@users.noreply.github.com>
2023-10-24 05:05:01 +0200

    Add files via upload

---
+++ DISM/Module_WindowsImageTools.ps1
@@ -0,0 +1,20 @@
+﻿Install-Module -Name WindowsImageTools
+Import-Module WindowsImageTools
+Get-Command -Module WindowsImageTools
+
+#Get Windows Editions with DISM
+dism /Get-WimInfo /wimFile:E:\sources\install.wim
+#dism /Get-WimInfo /wimFile:C:\Lab\Kurs\Windows1022H2Education.wim
+
+
+
+#Example create vhdx with Windows 10 22H2 Education
+
+Convert-WIM2VHD -Path C:\LAB\Kurs\win10-22H2.vhdx -SourcePath E:\Sources\install.wim `
+-index 1 -Size 40GB -DiskLayout UEFI -Dynamic
+
+
+#Example create vhdx with Windows 10 22H2 Education
+
+Convert-WIM2VHD -Path C:\LAB\Kurs\win10-22H2.vhdx -SourcePath C:\Lab\Kurs\Windows1022H2Education.wim `
+-index 1 -Size 40GB -DiskLayout UEFI -Dynamic
